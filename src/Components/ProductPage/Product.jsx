e5f7f361730fe6c5298fa9530c3e1e18670716a4
Mahesh Rayate <Mahesh.Rayate@ril.com>
2025-09-02 15:02:44 +0530

    Updated Code

--- src/Components/ProductPage/Product.jsx
+++ src/Components/ProductPage/Product.jsx
@@ -29,21 +29,21 @@ const Product = () => {
     <div>
       <div className="flex items-start product-container">
         <ProductPageFilter />
-        <div className=" min-w-full md:min-w-[75%]">
-          <div className="w-full px-3 grid grid-cols-2 gap-x-1 gap-y-1 lg:grid-cols-4 lg:gap-x-4 lg:gap-y-5">
-            {isLoading ? (
-              <div className="min-h-screen flex justify-center items-center">
-                <LoaderSpinner />
-              </div>
-            ) : (
-              data?.data?.products.map((product) => {
+        {isLoading ? (
+          <div className="min-h-100 flex justify-center items-center lg:min-w-5/10 w-full">
+            <LoaderSpinner />
+          </div>
+        ) : (
+          <div className=" min-w-full md:min-w-[75%]">
+            <div className="w-full px-3 grid grid-cols-2 gap-x-1 gap-y-1 lg:grid-cols-4 lg:gap-x-4 lg:gap-y-5">
+              {data?.data?.products.map((product) => {
                 const id = nanoid();
                 return <ProductCard key={id} {...product} />;
-              })
-            )}
+              })}
+            </div>
+            <PaginationContainer />
           </div>
-          <PaginationContainer />
-        </div>
+        )}
       </div>
       {/* Mobile Filter and Sort By */}(
       <div>
